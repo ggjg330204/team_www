168dc813f49ee010f867ff6d4160b319be7690ff
HamaPr <1234enrud@gmail.com>
2025-11-20 16:27:15 +0900

    Delete modules/DB directory

--- modules/DB/cdn/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cdn_endpoint_hostname" {
-  description = "The Hostname of the CDN Endpoint"
-  value       = "https://${azurerm_cdn_endpoint.main.fqdn}"
-}
-
-/*
-### 학습 포인트 (CDN Outputs)
-- **cdn_endpoint_hostname**: 전 세계에 배포된 CDN의 접속 주소입니다. 이미지 링크를 이걸로 바꾸면 엄청 빨라집니다.
-*/
