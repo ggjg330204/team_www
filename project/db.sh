15cc22f5ec703618a4b1b0a787e08972990ce173
hybit0527-citizen <hybit0527@gmail.com>
2025-11-21 18:12:13 +0900

    Update db.sh
    hyb 부분을 www로 바꿨습니다

--- project/db.sh
+++ project/db.sh
@@ -3,5 +3,5 @@ setenforce 0
 grubby --update-kernel ALL --args selinux=0
 dnf install -y mysql-server
 systemctl enable --now mysqld
-mysql -uroot -e "CREATE USER 'www'@'%' IDENTIFIED BY 'It12345!'; GRANT ALL PRIVILEGES ON *.* TO 'hyb'@'%'; CREATE DATABASE wordpress;"
+mysql -uroot -e "CREATE USER 'www'@'%' IDENTIFIED BY 'It12345!'; GRANT ALL PRIVILEGES ON *.* TO 'www'@'%'; CREATE DATABASE wordpress;"
 
