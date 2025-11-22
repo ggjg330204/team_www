efb49e7caad5f9a334ee8cc82e8b322cc8084e41
HamaPr <1234enrud@gmail.com>
2025-11-22 23:38:33 +0900

    Delete 04_t1_www_11.22 directory

--- 04_t1_www_11.22/modules/Compute/99_out.tf
+++
@@ -1,8 +0,0 @@
-output "vmss_id" {
-  value = azurerm_linux_virtual_machine_scale_set.vmss.id
-}
-
-output "vm_principal_id" {
-  description = "Principal ID of the Web VM's managed identity"
-  value       = azurerm_linux_virtual_machine.webvm.identity[0].principal_id
-}
