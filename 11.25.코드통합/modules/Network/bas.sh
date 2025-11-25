795b66b7c22e5092d3a12e70216c0df9c9c1e82b
HamaPr <1234enrud@gmail.com>
2025-11-26 00:59:14 +0900

    Delete 11.25.코드통합 directory

--- 11.25.코드통합/modules/Network/bas.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-setenforce 0
-grubby --update-kernel ALL --args
-selinux=0
-dnf install -y mysql lynx
--- 11.25.코드통합/scripts/bas.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /home/${admin_username}/.ssh
-echo "${ssh_private_key}" > /home/${admin_username}/.ssh/id_rsa
-chmod 600 /home/${admin_username}/.ssh/id_rsa
-chown -R ${admin_username}:${admin_username} /home/${admin_username}/.ssh
-dnf install -y mysql
