cac31a997055af6c57dabac1952e2f13737a46f4
hjmscs4 <hjmscs4@gmail.com>
2025-11-25 14:51:58 +0900

    security_3차 nsg nsgsub 추가
    vmss 추가에 맞게 nsg subnet association 부분 변경함

---
+++ project/modules/Security/3차 nsg nsgsub/09_nsgsub.tf
@@ -0,0 +1,29 @@
+resource "azurerm_subnet_network_security_group_association" "www_asso_subnsg_bas" {
+  subnet_id                 = var.subnet_bas.id
+  network_security_group_id = var.www_nsg_ssh
+}
+
+resource "azurerm_subnet_network_security_group_association" "www_asso_subnsg_web1" {
+  subnet_id                 = var.subnet_web1.id
+  network_security_group_id = var.www_nsg_http
+}
+
+resource "azurerm_subnet_network_security_group_association" "www_asso_subnsg_web2" {
+  subnet_id                 = var.subnet_web2.id
+  network_security_group_id = var.www_nsg_http
+}
+
+resource "azurerm_subnet_network_security_group_association" "www_asso_subnsg_db" {
+  subnet_id                 = var.subnet_db
+  network_security_group_id = var.www_nsg_db
+}
+
+resource "azurerm_subnet_network_security_group_association" "www_sth_asso_subnsg_web1" {
+  subnet_id                 = var.sth_subnet_web1.id
+  network_security_group_id = var.www_sth_nsg_http.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "www_sth_asso_subnsg_vmss" {
+  subnet_id                 = var.sth_subnet_vmss.id
+  network_security_group_id = var.www_sth_nsg_http.id
+}
