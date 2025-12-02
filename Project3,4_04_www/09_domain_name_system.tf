4b42e985031da8c2e90706185fa7553cc7454689
HamaPr <1234enrud@gmail.com>
2025-12-03 08:50:16 +0900

    Add files via upload

---
+++ Project3,4_04_www/09_domain_name_system.tf
@@ -0,0 +1,77 @@
+resource "azurerm_dns_zone" "public" {
+  name                = "04www.cloud"
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    Environment = "Production"
+    Purpose     = "Public-DNS"
+    ManagedBy   = "Terraform"
+  }
+}
+resource "azurerm_dns_cname_record" "www_public" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.public.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  record              = module.storage.frontdoor_endpoint_hostname
+}
+
+# DNS validation for Front Door custom domain (www.04www.cloud)
+resource "azurerm_dns_txt_record" "www_validation" {
+  name                = "_dnsauth.www"
+  zone_name           = azurerm_dns_zone.public.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+
+  record {
+    value = "_g55emm2o3acpaiqkt80gl4b8x2sa4h0"
+  }
+}
+
+# Root domain A record (points to Load Balancer)
+resource "azurerm_dns_a_record" "root_public" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.public.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  records             = [module.network_central.lb_public_ip]
+}
+resource "azurerm_private_dns_zone" "private" {
+  name                = "04www.cloud"
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    Environment = "Production"
+    Purpose     = "Private-DNS"
+    ManagedBy   = "Terraform"
+  }
+}
+resource "azurerm_private_dns_zone_virtual_network_link" "link_vnet0" {
+  name                  = "link-vnet0"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.private.name
+  virtual_network_id    = module.network_central.vnet_id
+  registration_enabled  = true
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+resource "azurerm_private_dns_zone_virtual_network_link" "link_hub" {
+  name                  = "link-hub"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.private.name
+  virtual_network_id    = module.hub.hub_vnet_id
+  registration_enabled  = false
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+resource "azurerm_private_dns_a_record" "db_private" {
+  name                = "db"
+  zone_name           = azurerm_private_dns_zone.private.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  records             = [module.database.mysql_private_endpoint_ip]
+}
