c7aec15629093b51902979c7b8a1c34cff5693c7
HamaPr <1234enrud@gmail.com>
2025-12-05 10:18:47 +0900

    Delete Project3,4_04_www directory

--- Project3,4_04_www/modules/ContainerRegistry/99_outputs.tf
+++
@@ -1,9 +0,0 @@
-﻿output "acr_id" {
-  value = azurerm_container_registry.main.id
-}
-output "acr_name" {
-  value = azurerm_container_registry.main.name
-}
-output "acr_login_server" {
-  value = azurerm_container_registry.main.login_server
-}
