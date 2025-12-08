2d81f32d02c4a5b553b2e2cc559c1a6d8d91ae7b
HamaPr <1234enrud@gmail.com>
2025-12-08 09:08:58 +0900

    Delete Project3,4_04_www directory

--- Project3,4_04_www/00_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "04-t1-www-rg"
-    storage_account_name = "wwwstoragecphr"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_azuread_auth     = true
-  }
-}
--- Project3,4_04_www/modules/Governance/03_resource_lock.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_management_lock" "db_lock" {
-  name       = "mysql-delete-lock"
-  scope      = var.db_id
-  lock_level = "CanNotDelete"
-  notes      = "Critical Production Database - Deletion Prevented"
-}
