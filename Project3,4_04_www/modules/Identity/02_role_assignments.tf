98bea7d13cf27837473e5cafd420dcdf496028e7
HamaPr <1234enrud@gmail.com>
2025-12-09 15:42:52 +0900

    Update 02_role_assignments.tf

--- Project3,4_04_www/modules/Identity/02_role_assignments.tf
+++ Project3,4_04_www/modules/Identity/02_role_assignments.tf
@@ -10,6 +10,18 @@ resource "azurerm_role_assignment" "pm_sentinel" {
   principal_id         = data.azuread_user.pm.object_id
 }
 
+resource "azurerm_role_assignment" "pm_monitoring" {
+  scope                = var.rgid
+  role_definition_name = "Monitoring Contributor"
+  principal_id         = data.azuread_user.pm.object_id
+}
+
+resource "azurerm_role_assignment" "pm_keyvault" {
+  scope                = var.key_vault_id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azuread_user.pm.object_id
+}
+
 resource "azurerm_role_assignment" "arch_reader" {
   scope                = var.rgid
   role_definition_name = "Reader"
@@ -32,4 +44,10 @@ resource "azurerm_role_assignment" "sec_internal2_log_analytics" {
   scope                = var.log_analytics_workspace_id
   role_definition_name = "Log Analytics Reader"
   principal_id         = data.azuread_user.sec_internal2.object_id
+}
+
+resource "azurerm_role_assignment" "sec_external2_sentinel" {
+  scope                = var.log_analytics_workspace_id
+  role_definition_name = "Microsoft Sentinel Contributor"
+  principal_id         = data.azuread_user.sec_external2.object_id
 }
